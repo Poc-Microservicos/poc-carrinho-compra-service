2f7df809a05b3c5d05b5f4bdc92fcb066a5bcb3e
Renan Furtado <renan.furtado@wedotec.com.br>
2024-10-18 15:51:20 -0300

    Configurando repasse de trace

--- src/main/java/br/com/pupposoft/poc/monitoriamento/carrinhocompra/gateway/api/ProdutoApiServiceGateway.java
+++ src/main/java/br/com/pupposoft/poc/monitoriamento/carrinhocompra/gateway/api/ProdutoApiServiceGateway.java
@@ -5,47 +5,57 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.pupposoft.poc.monitoriamento.carrinhocompra.domain.Produto;
 import br.com.pupposoft.poc.monitoriamento.carrinhocompra.exception.AcessoProdutoServiceException;
 import br.com.pupposoft.poc.monitoriamento.carrinhocompra.gateway.ProdutoGateway;
 import br.com.pupposoft.poc.monitoriamento.carrinhocompra.gateway.api.json.ProdutoJson;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ProdutoApiServiceGateway implements ProdutoGateway {
 
 	@Value("${poc.monitoramento.service.produto.base-url}")
 	private String produtoBaseUrl;
-	
+
+	private final WebClient.Builder webClientBuilder;
+
 	@Override
 	public List<Produto> obterPorIds(List<Long> idsProdutos) {
 
 		try {
-			RestClient restClient = RestClient.create();
+
 			final String url = produtoBaseUrl + "/poc/monitoramento/produto/v1/produtos/";
-			
-			
+
 			return idsProdutos.stream()
-			.map(id -> url + id)
-			.map(urlFinal -> restClient.get()
-						.uri(urlFinal)
-						.retrieve()
-						.body(ProdutoJson.class))
-			.map(this::mapJsonToDomain)
-			.toList();
+					.map(id -> url + id)
+					.map(this::callService)
+					.map(this::mapJsonToDomain)
+					.toList();
 
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			throw new AcessoProdutoServiceException();
 		}
-		
+
+	}
+
+	private ProdutoJson callService(String url) {
+		WebClient webClient = webClientBuilder.baseUrl(url).build();
+		return webClient
+				.get()
+				.retrieve()
+				.bodyToMono(ProdutoJson.class)
+				.block(); // 'block()' é utilizado para realizar a chamada de forma síncrona.
+
 	}
 
 	private Produto mapJsonToDomain(ProdutoJson produtoJson) {
 		return new Produto(produtoJson.getId(), BigDecimal.valueOf(produtoJson.getPreco()));
 	}
-	
+
 }
